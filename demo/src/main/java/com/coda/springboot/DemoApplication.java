365470efe177469f9c5954bfd982692196e3c23f
munish <munish7771@gmail.com>
2019-05-07 18:11:01 +0530

    added boot app
    sproing boot app added

--- demo/src/main/java/com/coda/springboot/DemoApplication.java
+++ demo/src/main/java/com/coda/springboot/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.coda.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//import com.coda.springboot.models.user;
+import com.coda.springboot.repository.userrepository;
+
 @SpringBootApplication
 public class DemoApplication {
-
+    @Autowired
+    private static userrepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+//		mongo_get();
+//	}
+//	public static void mongo_get() {
+//		user u = repository.findUserByName("munish").get(0);
+//		if(u!=null) {
+//		System.out.print(u.password);
+//		}
 	}
-
 }
